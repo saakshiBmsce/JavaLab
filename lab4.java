470b10ad4f0150a688761c258f52743a348835f8
saakshiBmsce <53811241+saakshiBmsce@users.noreply.github.com>
2019-10-12 13:01:19 +0530

    Add files via upload

--- lab4.java
+++ lab4.java
@@ -1,53 +1,54 @@
 import java.util.*;
-class Book
-{
-	String name;
-	String author;
-	double price;
-	int num_pages;
-	public void get_data()
-	{
-		Scanner scan=new Scanner(System.in);
-		System.out.println("Enter books name:  ");
-		this.name=scan.next();
-		System.out.println("Enter author's name:  ");
-		this.author=scan.next();
-		System.out.println("Enter book's price:  ");
-		this.price=scan.nextDouble();
-		System.out.println("Enter the number of pages of book:  ");
-		this.num_pages=scan.nextInt();
-	}
-	
-	public String toString()
-	{
-		String s=new String();
-		s="Book description: \n1)Book name: "+this.name+"\n2)Author's name: "+
-		 this.author+"\n3)Price: "+this.price+"\n4)no of pages:  "+this.num_pages+"";
-		
-		return s;
-	}
-
-}
-
 class lab4
 {
 	public static void main(String args[])
 	{
 		Scanner scan=new Scanner(System.in);
-		System.out.println("Enetr the number of objects:  ");
-		int n=scan.nextInt();
-		Book b[]=new Book[n];
-		for(int i=0;i<n;i++)
-		{
-			b[i]=new Book();
-		}
-		for(int i=0;i<n;i++)
-		{
-			System.out.println("Enter "+i+" book details: ");
-			b[i].get_data();
-
-			System.out.println(b[i]);
-		}
+		System.out.println("Enter a and b");
+		int a=scan.nextInt();int b=scan.nextInt();
 		
+		Circle c=new Circle(a,b);
+		Rectangle r=new Rectangle(a,b);
+		Triangle t=new Triangle(a,b);
+		c.area();r.area();t.area();
 	}
 }
+abstract class Shape
+{
+	int a,b;
+	
+	public abstract void area();
+}
+
+class Circle extends Shape
+{	
+	Circle(){}
+	Circle(int a,int b){this.a=a;this.b=b;}
+	public void area()
+	{
+		System.out.println("The area of Circle is  "+(3.14*a*a));
+	}
+}
+
+class Rectangle extends Shape
+{	
+	Rectangle(){}
+	Rectangle(int a,int b){this.a=a;this.b=b;}
+	public void area()
+	{
+		System.out.println("The area of Rectangle is  "+(b*a));
+	}
+}
+
+class Triangle extends Shape
+{	
+	
+	Triangle(){}
+	Triangle(int a,int b){this.a=a;this.b=b;}
+	public void area()
+	{
+		System.out.println("The area of Triangle is  "+(0.5*b*a));
+	}
+}
+
+
